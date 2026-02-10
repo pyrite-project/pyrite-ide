5e635a255672dbbd1af619d721b2b284bf9ae583
Can1425 <myqf1425@126.com>
2026-02-10 22:53:28 +0800

    feat(tools): add board manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:pyrite_ide/app/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pyrite_ide/core/services/app.dart';
 import 'package:pyrite_ide/core/services/pylsp/main.dart';
 import 'package:pyrite_ide/features/window.dart';
-import 'package:pyrite_ide/core/services/android_env_deployer/core.dart';
-import 'package:pyrite_ide/src/rust/api/main.dart';
 import 'package:pyrite_ide/src/rust/frb_generated.dart';
 
 // PyriteIDE: Hello World.
 void main() async {
   container = ProviderContainer();
   UseWindow().init();
-
   await RustLib.init();
-  var t = await getPortList();
-  print(t[0].portName);
 
   runApp(
     UncontrolledProviderScope(container: container, child: const PyriteIDE()),
--- lib/pages/tools/main.dart
+++ lib/pages/tools/main.dart
@@ -1,29 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pyrite_ide/core/services/tools/board_manager.dart';
 import 'package:pyrite_ide/shared/studio_text.dart';
+import 'package:pyrite_ide/src/rust/api/main.dart';
 
-class Tools extends StatelessWidget {
+class Tools extends ConsumerWidget {
   const Tools({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          const SliverAppBar.large(title: UseText("工具")),
-          SliverToBoxAdapter(
-            child: Column(
+      appBar: AppBar(title: Text("工具")),
+      body: DefaultTabController(
+        length: 1,
+        child: Column(
+          children: [
+            Row(
               children: [
-                Icon(
-                  Icons.border_clear,
-                  size: 60,
-                  color: Theme.of(context).colorScheme.secondary,
+                Expanded(
+                  child: TabBar(
+                    tabAlignment: TabAlignment.start,
+                    tabs: [Tab(text: "设备管理", height: 30)],
+                    isScrollable: true,
+                  ),
                 ),
-                UseText("暂无工具", color: Theme.of(context).colorScheme.secondary),
               ],
             ),
-          ),
-        ],
+
+            Expanded(child: TabBarView(children: [buildBoardManager(ref)])),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget buildBoardManager(WidgetRef ref) {
+    updatePortList(ref);
+    return CustomScrollView(
+      slivers: [
+        SliverList.builder(
+          itemCount: ref.watch(portList).length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(ref.watch(portList)[index].portName),
+              subtitle: Text("点击以尝试连接"),
+              onTap: () {
+                ref.read(selectedPort.notifier).state = ref.read(
+                  portList,
+                )[index];
+
+                connectPort(portName: ref.read(selectedPort)!.portName);
+              },
+            );
+          },
+        ),
+      ],
     );
   }
 }
