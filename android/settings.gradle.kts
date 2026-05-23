04371aa8b51a1f966b384d2acabf3c11dc1f71b3
Can1425 <myqf1425@126.com>
2026-05-24 00:07:47 +0800

    build: v0.2 release preparation

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,14 +11,6 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/central") }
-        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
-        maven { setUrl("https://maven.aliyun.com/nexus/content/groups/public/") }
-        maven { setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
-        maven { setUrl("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
         google()
         mavenCentral()
         gradlePluginPortal()
